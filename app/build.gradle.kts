997b153c37d9e4fe64c7816d58534db2bad395a9
TechyTR <hasanakmercan05@gmail.com>
2026-09-20 15:33:11 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling-preview)
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation(libs.androidx.compose.material3)
 
     implementation("dev.rikka.shizuku:api:13.1.5")
